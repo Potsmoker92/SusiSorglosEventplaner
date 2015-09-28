5e7e83f0b29290b18e840d697f17673ba455bcbe
Karl Schmidt <Karl.Schmidt@team.cortado.com>
2015-09-28 11:12:29 +0200

    Initial Push

---
+++ SusiSorglosEventplaner/SusiSorglosEventplaner/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SusiSorglosEventplaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageOverview = new System.Windows.Forms.TabPage();
+            this.tabPagePerson = new System.Windows.Forms.TabPage();
+            this.tabPageEvent = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageOverview);
+            this.tabControl1.Controls.Add(this.tabPagePerson);
+            this.tabControl1.Controls.Add(this.tabPageEvent);
+            this.tabControl1.Location = new System.Drawing.Point(-1, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(837, 350);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageOverview
+            // 
+            this.tabPageOverview.Location = new System.Drawing.Point(4, 22);
+            this.tabPageOverview.Name = "tabPageOverview";
+            this.tabPageOverview.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageOverview.Size = new System.Drawing.Size(829, 329);
+            this.tabPageOverview.TabIndex = 0;
+            this.tabPageOverview.Text = "Overview";
+            this.tabPageOverview.UseVisualStyleBackColor = true;
+            // 
+            // tabPagePerson
+            // 
+            this.tabPagePerson.Location = new System.Drawing.Point(4, 22);
+            this.tabPagePerson.Name = "tabPagePerson";
+            this.tabPagePerson.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPagePerson.Size = new System.Drawing.Size(829, 324);
+            this.tabPagePerson.TabIndex = 1;
+            this.tabPagePerson.Text = "Person";
+            this.tabPagePerson.UseVisualStyleBackColor = true;
+            // 
+            // tabPageEvent
+            // 
+            this.tabPageEvent.Location = new System.Drawing.Point(4, 22);
+            this.tabPageEvent.Name = "tabPageEvent";
+            this.tabPageEvent.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageEvent.Size = new System.Drawing.Size(829, 324);
+            this.tabPageEvent.TabIndex = 2;
+            this.tabPageEvent.Text = "Event";
+            this.tabPageEvent.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 357);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageOverview;
+        private System.Windows.Forms.TabPage tabPagePerson;
+        private System.Windows.Forms.TabPage tabPageEvent;
+    }
+}
+
