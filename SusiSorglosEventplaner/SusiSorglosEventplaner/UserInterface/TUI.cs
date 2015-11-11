bdd235962156cf9e7ac157b822361e1aae91cd60
potsmoker92 <smoki-mc-pot@web.de>
2015-11-11 19:56:34 +0100

    implemented TUI change funtions / user interaction

--- SusiSorglosEventplaner/SusiSorglosEventplaner/UserInterface/TUI.cs
+++ SusiSorglosEventplaner/SusiSorglosEventplaner/UserInterface/TUI.cs
@@ -40,8 +40,8 @@ namespace SusiSorglosEventplaner
                 AllocConsole();
             }
 
-            fachkonzept = new FachkonzeptSortiert(new DataManagement());
-            //fachkonzept = new FachkonzeptUnsortiert(new DataManagement());
+            //fachkonzept = new FachkonzeptSortiert(new DataManagement());
+            fachkonzept = new FachkonzeptUnsortiert(new DataManagement());
 
             ShowOption();
            
@@ -82,65 +82,65 @@ namespace SusiSorglosEventplaner
                 return; //quit 
             }
 
-            if(selectedAction == 1 )
+            if(selectedAction == 1 ) //anzeigen 
             {
-                if (selectedType == 1)
+                if (selectedType == 1) //benutzer 
                 {
                   foreach(User usr in  fachkonzept.getAllusers())
                   {
-                      Console.WriteLine("ID {0,4} Vorname {1,10} , Nachname {2,10}", usr.userID, usr.strVorname, usr.strNachname);
+                      Console.WriteLine("ID: {0,4} | Vorname: {1,10} | Nachname: {2,10}", usr.userID, usr.strVorname, usr.strNachname);
                   }
                   Console.WriteLine("Drücken Sie irgend eine Taste um zum Anfangsmenu zurrückzukehren.");
                   Console.ReadKey();
                 }
-                if (selectedType == 2)
+                if (selectedType == 2)//event
                 {
                     foreach (Event ent in fachkonzept.getAllEvents())
                     {
-                        Console.WriteLine("ID {0,4} Name {1,10} , Ort {2,10}", ent.eventID, ent.strEventname,ent.strEventLocation);
+                        Console.WriteLine("ID: {0,4} | Name: {1,10} | Ort: {2,10}", ent.eventID, ent.strEventname,ent.strEventLocation);
                     }
                     Console.WriteLine("Drücken Sie irgend eine Taste um zum Anfangsmenu zurrückzukehren.");
                     Console.ReadKey();
                 }
             }
 
-            if (selectedAction == 2)
+            if (selectedAction == 2)//hinzufuegen
             {
-                if (selectedType == 1)
+                if (selectedType == 1)//benutzer
                 {
 
                     User newUser = requetsUser();
                     fachkonzept.insertUser(newUser);
                 }
-                if (selectedType == 2)
+                if (selectedType == 2)//event
                 {
                     Event newEvent = requetsEvent();
                     fachkonzept.insertEvent(newEvent);
                 }
             }
 
-            if (selectedAction == 3)
+            if (selectedAction == 3) //löschen
             {
-                if (selectedType == 1)
+                if (selectedType == 1)//benutzer
                 {
                     int deletID = requestUserID();
                     fachkonzept.deleteUser(deletID);
                 }
-                if (selectedType == 2)
+                if (selectedType == 2)//event
                 {
                     Event deleteEvent = requestdeleteEvent();
                     fachkonzept.deleteEvent(deleteEvent);
                 }
             }
 
-            if (selectedAction == 4)
+            if (selectedAction == 4)//bearbeitern
             {
-                if (selectedType == 1)
+                if (selectedType == 1)//benutzer
                 {
                     User changedUser = requestChangedUser();
                     fachkonzept.updateUser(changedUser);
                 }
-                if (selectedType == 2)
+                if (selectedType == 2)//event
                 {
                     // was not implemented by tim / tino
                 }
@@ -187,21 +187,33 @@ namespace SusiSorglosEventplaner
                 result.strEventLocation = Console.ReadLine();
             } while (result.strEventLocation == "");
 
+            // fehlt noch datumseingabe
             return result;
         }
 
         private int requestUserID()
         {
-            return -1;
+            int id = -1;
+            Console.Clear();
+            Console.Write("Geben Sie die USer-ID ein:");
+            string input = Console.ReadLine();
+            Int32.TryParse(input, out id);
+            return id;
         }
         private Event requestdeleteEvent()
         {
-            return new Event();
+            Console.Clear();
+            Console.Write("Geben Sie die Event-ID ein von dem Event das gelöscht werden soll: ");
+            string input = Console.ReadLine();
+            int id = -1;
+            Int32.TryParse(input, out id);
+
+            return new Event() {eventID=id };
         }
 
         private User requestChangedUser()
         {
-            return new User();
+            return requetsUser();
         }
      public static bool AttachToConsole()
         {
